dd10c9191ef05417c6705fb7095eecebb34ef678
sebas <juaagucas@alu.edu.gva.es>
2026-02-11 15:14:42 +0100

    ansible examen

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("¡Hola desde Ansible y GitHub Actions!");
+  res.send("Examen DAW Despliegue inicial");
 });
 
 app.listen(port, () => {
